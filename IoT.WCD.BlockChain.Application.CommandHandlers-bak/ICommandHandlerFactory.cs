2da52b797a2326fac47515f3ce77720fe28014f0
kingdom-0 <kingdom_0@foxmail.com>
2019-10-14 00:55:16 +0800

    Remove redundant bak projects.

--- IoT.WCD.BlockChain.Application.CommandHandlers-bak/ICommandHandlerFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using IoT.WCD.BlockChain.Application.Commands;
-
-namespace IoT.WCD.BlockChain.Application.CommandHandlers
-{
-    public interface ICommandHandlerFactory
-    {
-        ICommandHandler<T> GetHandler<T>() where T : Command;
-    }
-}
