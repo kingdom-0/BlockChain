411765826c8d525590889243af2ba9f81345bf94
kingdom-0 <kingdom_0@foxmail.com>
2019-11-10 22:27:53 +0800

    Remove redundant files.

--- IoT.WCD.BlockChain.Application.CommandHandlers/ICommandHandler.cs
+++ IoT.WCD.BlockChain.Application.CommandHandlers/ICommandHandler.cs
@@ -6,9 +6,4 @@ namespace IoT.WCD.BlockChain.Application.CommandHandlers
     {
         void Execute(TCommand command);
     }
-
-    public interface ICommandHandler<in TCommand, out TResult> where TCommand : ICommand
-    {
-        TResult Execute(TCommand command);
-    }
 }
--- IoT.WCD.BlockChain.Infrastructure/SubmitResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IoT.WCD.BlockChain.Infrastructure
-{
-    public class SubmitResult
-    {
-        public bool IsSucceed { get; set; } = true;
-
-        public string Message { get; set; }
-    }
-}
